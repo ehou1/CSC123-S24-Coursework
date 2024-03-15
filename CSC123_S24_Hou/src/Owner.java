37dc33904a1f7a5df21a973eb951522c3cbfe1fa
ehou1 <157663403+ehou1@users.noreply.github.com>
2024-03-14 19:19:53 -0700

    Add files via upload

---
+++ CSC123_S24_Hou/src/Owner.java
@@ -0,0 +1,12 @@
+package DMV;
+
+class Owner {
+    private String name;
+    // Other owner details
+
+    public Owner(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
+}
