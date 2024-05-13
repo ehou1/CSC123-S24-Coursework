c28a0f3c8f6695087528bdaa6c56c35221031a17
ehou1 <157663403+ehou1@users.noreply.github.com>
2024-05-12 20:45:28 -0700

    Add files via upload

---
+++ CSC123_S24_Hou/assignmentFour/src/javaIO/MyScanner.java
@@ -0,0 +1,24 @@
+package javaIO;
+
+public class MyScanner {
+    public MyScanner(String input) {
+    }
+
+    public void next() {
+        // Logic to read and return the next token
+    }
+
+    public int nextInt() {
+		return 0;
+        // Logic to read and return the next integer
+    }
+
+    // Implement other methods similarly
+
+    public boolean hasNext() {
+		return false;
+        // Logic to check if there's more input available
+    }
+
+    // Implement other hasNextX methods similarly
+}
